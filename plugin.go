8c479c44db7c0668c5675af5e27a250b706db388
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-02-24 00:07:52 +0100

    feat: Add star notification support and improve GitHub plugin functionality
    - Implement star tracking feature with optional star notifications
    - Add `watchStars` configuration option
    - Create `checkStars()` method to fetch and notify about new repository stars
    - Enhance error logging for GitHub API requests
    - Update plugin configuration to support star tracking
    - Refactor config validation and default configuration methods

--- plugin.go
+++ plugin.go
@@ -25,28 +25,59 @@ type GithubNotification struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-func GetGotifyPluginInfo() plugin.Info {
-	return plugin.Info{
-		ModulePath:  "github.com/gotify/github-plugin",
-		Version:     "1.0.0",
-		Author:      "Your Name",
-		Website:     "https://github.com",
-		Description: "Github notification plugin for Gotify",
-		License:     "MIT",
-		Name:        "gotify-github-plugin",
+type MyPlugin struct {
+	ctx               plugin.UserContext
+	enabled           bool
+	stopChannel       chan struct{}
+	githubToken       string
+	pollInterval      time.Duration
+	lastCheckTime     time.Time
+	lastStarCheckTime time.Time
+	appID             uint
+	appToken          string
+	watchStars        bool
+	msgHandler        plugin.MessageHandler
+}
+
+func (c *MyPlugin) DefaultConfig() any {
+	return &struct {
+		Token       string `json:"token"`
+		Interval    int    `json:"interval"`
+		AppToken    string `json:"apptoken"`
+		WatchStars  bool   `json:"watchStars"`
+		Description string `json:"description"`
+	}{
+		Token:       "",
+		Interval:    60,
+		AppToken:    "",
+		WatchStars:  false,
+		Description: "Enter GitHub token, polling interval (seconds), Gotify application token, and enable star notifications",
 	}
 }
 
-type MyPlugin struct {
-	ctx           plugin.UserContext
-	enabled       bool
-	stopChannel   chan struct{}
-	githubToken   string
-	pollInterval  time.Duration
-	lastCheckTime time.Time
-	appID         uint
-	appToken      string
-	msgHandler    plugin.MessageHandler
+func (c *MyPlugin) ValidateAndSetConfig(cfg interface{}) error {
+	b, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	var conf struct {
+		Token       string `json:"token"`
+		Interval    int    `json:"interval"`
+		AppToken    string `json:"apptoken"`
+		WatchStars  bool   `json:"watchStars"`
+		Description string `json:"description"`
+	}
+	if err = json.Unmarshal(b, &conf); err != nil {
+		return err
+	}
+	if conf.Token == "" {
+		return fmt.Errorf("GitHub token is required")
+	}
+	c.githubToken = conf.Token
+	c.pollInterval = time.Duration(conf.Interval) * time.Second
+	c.appToken = conf.AppToken
+	c.watchStars = conf.WatchStars
+	return nil
 }
 
 func (c *MyPlugin) Enable() error {
@@ -73,11 +104,13 @@ func (c *MyPlugin) Disable() error {
 func (c *MyPlugin) startPolling() {
 	ticker := time.NewTicker(c.pollInterval)
 	defer ticker.Stop()
-
 	for {
 		select {
 		case <-ticker.C:
 			c.checkNotifications()
+			if c.watchStars {
+				c.checkStars()
+			}
 		case <-c.stopChannel:
 			return
 		}
@@ -88,26 +121,25 @@ func (c *MyPlugin) checkNotifications() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://api.github.com/notifications", nil)
 	if err != nil {
-		log.Printf("error creating request: %v", err)
+		log.Printf("error creating github notifications request: %v", err)
 		return
 	}
 	req.Header.Add("Authorization", "token "+c.githubToken)
 	req.Header.Add("Accept", "application/vnd.github.v3+json")
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Printf("error executing request: %v", err)
+		log.Printf("error executing github notifications request: %v", err)
 		return
 	}
 	defer resp.Body.Close()
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("error reading response body: %v", err)
+		log.Printf("error reading github notifications response: %v", err)
 		return
 	}
 	var notifications []GithubNotification
 	if err := json.Unmarshal(body, &notifications); err != nil {
-		log.Printf("error unmarshalling json: %v", err)
+		log.Printf("error unmarshalling github notifications: %v", err)
 		return
 	}
 	for _, notification := range notifications {
@@ -117,49 +149,92 @@ func (c *MyPlugin) checkNotifications() {
 				Message:  fmt.Sprintf("New notification in %s", notification.Repository.FullName),
 				Priority: 2,
 			}
-			err := c.msgHandler.SendMessage(*msg)
-			if err != nil {
-				log.Printf("error sending message: %v", err)
+			if err := c.msgHandler.SendMessage(*msg); err != nil {
+				log.Printf("error sending github notification: %v", err)
 			} else {
-				log.Printf("sent notification: %s", notification.Subject.Title)
+				log.Printf("sent github notification: %s", notification.Subject.Title)
 			}
 		}
 	}
 	c.lastCheckTime = time.Now()
 }
 
-func (c *MyPlugin) GetDefaultConfig() any {
-	return struct {
-		Token       string `json:"token"`
-		Interval    int    `json:"interval"`
-		AppToken    string `json:"apptoken"`
-		Description string `json:"description"`
-	}{
-		Token:       "",
-		Interval:    60,
-		AppToken:    "",
-		Description: "Enter your GitHub token, polling interval (in seconds), and Gotify application token to manage app settings, e.g. avatar image",
+func (c *MyPlugin) checkStars() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
+	if err != nil {
+		log.Printf("error creating repos request: %v", err)
+		return
 	}
+	req.Header.Add("Authorization", "token "+c.githubToken)
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("error executing repos request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	var repos []struct {
+		FullName string `json:"full_name"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&repos); err != nil {
+		log.Printf("error decoding repos: %v", err)
+		return
+	}
+	for _, repo := range repos {
+		repoURL := fmt.Sprintf("https://api.github.com/repos/%s/stargazers", repo.FullName)
+		req, err := http.NewRequest("GET", repoURL, nil)
+		if err != nil {
+			log.Printf("error creating stargazers request for %s: %v", repo.FullName, err)
+			continue
+		}
+		req.Header.Add("Authorization", "token "+c.githubToken)
+		req.Header.Add("Accept", "application/vnd.github.v3.star+json")
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("error executing stargazers request for %s: %v", repo.FullName, err)
+			continue
+		}
+		var stars []struct {
+			StarredAt time.Time `json:"starred_at"`
+			User      struct {
+				Login string `json:"login"`
+			} `json:"user"`
+		}
+		if err := json.NewDecoder(resp.Body).Decode(&stars); err != nil {
+			log.Printf("error decoding stargazers for %s: %v", repo.FullName, err)
+			resp.Body.Close()
+			continue
+		}
+		resp.Body.Close()
+		for _, star := range stars {
+			if star.StarredAt.After(c.lastStarCheckTime) {
+				msg := &plugin.Message{
+					Title:    "New Star",
+					Message:  fmt.Sprintf("Repo %s received a star from %s at %s", repo.FullName, star.User.Login, star.StarredAt.Format(time.RFC1123)),
+					Priority: 2,
+				}
+				if err := c.msgHandler.SendMessage(*msg); err != nil {
+					log.Printf("error sending star notification: %v", err)
+				} else {
+					log.Printf("sent star notification for repo %s", repo.FullName)
+				}
+			}
+		}
+	}
+	c.lastStarCheckTime = time.Now()
 }
 
-func (c *MyPlugin) ValidateConfig(config any) error {
-	b, err := json.Marshal(config)
-	if err != nil {
-		return err
+func GetGotifyPluginInfo() plugin.Info {
+	return plugin.Info{
+		ModulePath:  "github.com/0pandadev/gotify-github-plugin",
+		Version:     "1.0.0",
+		Author:      "PandaDEV",
+		Website:     "https://github.com",
+		Description: "Github notification plugin for Gotify",
+		License:     "MIT",
+		Name:        "gotify-github-plugin",
 	}
-	var conf struct {
-		Token       string `json:"token"`
-		Interval    int    `json:"interval"`
-		AppToken    string `json:"apptoken"`
-		Description string `json:"description"`
-	}
-	if err = json.Unmarshal(b, &conf); err != nil {
-		return err
-	}
-	if conf.Token == "" {
-		return fmt.Errorf("GitHub token is required")
-	}
-	return nil
 }
 
 func NewGotifyPluginInstance(ctx plugin.UserContext) plugin.Plugin {
@@ -176,56 +251,11 @@ func (c *MyPlugin) SetMessageHandler(h plugin.MessageHandler) {
 }
 
 func (c *MyPlugin) ApplyConfig(config any) error {
-	if configMap, ok := config.(map[string]any); ok {
-		if token, exists := configMap["token"].(string); exists {
-			c.githubToken = token
-		}
-		if interval, exists := configMap["interval"].(float64); exists {
-			c.pollInterval = time.Duration(interval) * time.Second
-		}
-	}
-	return nil
+	return c.ValidateAndSetConfig(config)
 }
 
 func (c *MyPlugin) GetDisplay(location *url.URL) string {
 	return "Configure your GitHub token and polling interval below to receive notifications"
-}
-
-func (c *MyPlugin) DefaultConfig() any {
-	return &struct {
-		Token       string `json:"token"`
-		Interval    int    `json:"interval"`
-		AppToken    string `json:"apptoken"`
-		Description string `json:"description"`
-	}{
-		Token:       "",
-		Interval:    60,
-		AppToken:    "",
-		Description: "Enter your GitHub token, polling interval (in seconds), and Gotify application token to manage app settings, e.g. avatar image",
-	}
-}
-
-func (c *MyPlugin) ValidateAndSetConfig(cfg interface{}) error {
-	b, err := json.Marshal(cfg)
-	if err != nil {
-		return err
-	}
-	var conf struct {
-		Token       string `json:"token"`
-		Interval    int    `json:"interval"`
-		AppToken    string `json:"apptoken"`
-		Description string `json:"description"`
-	}
-	if err = json.Unmarshal(b, &conf); err != nil {
-		return err
-	}
-	if conf.Token == "" {
-		return fmt.Errorf("GitHub token is required")
-	}
-	c.githubToken = conf.Token
-	c.pollInterval = time.Duration(conf.Interval) * time.Second
-	c.appToken = conf.AppToken
-	return nil
 }
 
 func main() {
